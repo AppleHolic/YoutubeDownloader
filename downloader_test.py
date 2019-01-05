579d6a0dc0678b370696f6c83eac68e423fde231
june-one <june.one@kakaobrain.com>
2019-01-05 17:48:23 +0900

    edit test file path

--- downloader_test.py
+++ downloader_test.py
@@ -6,12 +6,12 @@ from .downloader import download_youtube
 class YoutubeDownloadTest(unittest.TestCase):
 
     def test_download(self):
-        if os.path.exists('./download_test/test.mp3'):
-            os.remove('./download_test/test.mp3')
+        if os.path.exists('./download/test.mp3'):
+            os.remove('./download/test.mp3')
         link = 'https://www.youtube.com/watch?v=HI_afSFJXAs'
         check_file = download_youtube(link, 'test')
         self.assertTrue(check_file)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
